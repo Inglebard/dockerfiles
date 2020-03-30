51c8186fa3341bf12abbbba9054734c7e524c2bd
David Inglebard RICQ <davidricq87@orange.fr>
2020-03-30 11:37:39 +0200

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cd ${ROOT_WWW_PATH}/assets/frontend/bundle \
-../../../indexer > .index-xhr \
-cd ${ROOT_WWW_PATH}/assets/cores \
-../../indexer > .index-xhr \
+cd ${ROOT_WWW_PATH}/assets/frontend/bundle
+../../../indexer > .index-xhr
+cd ${ROOT_WWW_PATH}/assets/cores
+../../indexer > .index-xhr
 cd ${ROOT_WWW_PATH}
 nginx -g "daemon off;"
